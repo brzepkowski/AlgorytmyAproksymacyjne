a3a158c22ecffc12f7c557766d0a053baecda5b6
Kwaidan00 <aleksander.spyra@gmail.com>
2018-01-13 21:19:28 +0100

    Multifit was implemented. New testCases

--- Experiments.jl
+++ Experiments.jl
@@ -4,25 +4,8 @@ include("EnableLocalModules.jl")
 using TestCases
 using LargestProcessingTime
 using LocalSearchHeuristic
-using IntegerProgramming
+using ExoticAlgorithms
 
-function zkAlgorithm(times, m, k)
-  machines = zeros(Int64, m)
-  sorted = sort(times, rev=true)
-  kBests = sorted[1:k]
-  assignment, _ = integerProgramming(kBests, m)
-  for i ∈ 1:m
-    if length(assignment[i]) != 0
-      machines[i] = sum(x -> kBests[x], assignment[i])
-    end
-  end
-  for p in k+1:length(sorted)
-    index = indmin(machines)
-    push!(assignment[index], p)
-    machines[index] += sorted[p]
-  end
-  return assignment, maximum(machines)
-end
 
 function testSet(machines, testCaseGenerator :: Function, farg)
   for i ∈ 10:10:100
@@ -32,22 +15,23 @@ function testSet(machines, testCaseGenerator :: Function, farg)
     a1 = listSchedulingAlgorithm(times, machines)
     a2 = largestProcessingTime(times, machines)
     a3 = zkAlgorithm(times, machines, 5)
-    a4 = localSearchHeuristic(times, machines)
+    a4 = multifit(times, machines, farg)
+    a5 = localSearchHeuristic(times, machines)
     # a5 = tabu search
     # a6 = symulowane wyżarzanie
-    println("$i & $(testCase.makespan) & $(a1[2]) & $(a2[2]) & $(a3[2]) & $(a4[2]) \\\\ \\hline")
-    println("   & OPT & $((a1[2]/ms)) & $(a2[2]/ms) & $(a3[2]/ms) & $(a4[2]/ms)\\\\ \\hline")
+    println("$i & $(testCase.makespan) & $(a1[2]) & $(a2[2]) & $(a3[2]) & $(a4[2]) & $(a5[2])\\\\ \\hline")
+    println("   & OPT & $((a1[2]/ms)) & $(a2[2]/ms) & $(a3[2]/ms) & $(a4[2]/ms) & $(a5[2]/ms) \\\\ \\hline")
   end
 end
+
+18,6,20
  testSet(10, uniformTestCaseOptimum, 10)
-# testSet(3, easyTestCase, 10)
+
+ testSet(3, easyTestCase, 10)
 # testSet(3, uniformTestCaseOptimum, 10)
-# testSet(3, uniformTestCaseLowerBound, 10000)
+
+ testSet(3, uniformTestCaseLowerBound, 10)
 #
-# p = [3, 2, 6, 4, 5, 7, 9, 13, 4, 12, 10, 8, 22, 11, 8, 26, 14, 6, 17, 27, 11, 17, 26, 16, 7, 23, 15, 18, 15, 13]
-# listSchedulingAlgorithm(p, 3)
-# largestProcessingTime(p, 3)
-# zkAlgorithm(p, 3, 5)
-#
-#
-# testSet(3, myTestCase, 0)
+
+testSet(3, cauchyTestCaseLowerBound, 10)
+testSet(3, paretoTestCaseLowerBound, 10)
--- TestCases.jl
+++ TestCases.jl
@@ -3,11 +3,15 @@ include("EnableLocalModules.jl")
 module TestCases
 
 using IntegerProgramming
+using Distributions
+
 
 export easyTestCase
 export uniformTestCaseLowerBound
 export uniformTestCaseOptimum
-export expotentialTestCase
+export exponentialTestCase
+export cauchyTestCaseLowerBound
+export paretoTestCaseLowerBound
 
 struct TestCase
   time :: Vector{Int}
@@ -40,12 +44,27 @@ function uniformTestCaseOptimum(jobs, machines, maxJobTime)
   return TestCase(time, machines, makespan)
 end
 
-function expotentialTestCase(jobs, machines, f)
+function exponentialTestCase(jobs, machines, f)
   time = randexp(jobs)
   timeInt = map(x -> convert(Int64, round(1000*x)), time)
   _, makespan = integerProgramming(timeInt, machines)
   return TestCases.TestCase(timeInt, machines, makespan)
 end
+
+function paretoTestCaseLowerBound(jobs, machines, maxJobTime)
+  time = rand(Pareto(100, 100), jobs)
+  timeInt = map(x -> convert(Int64, abs(round(x))), time)
+  makespan = convert(Int64, round(linearProgrammingLowerBound(timeInt, machines)))
+  return TestCases.TestCase(timeInt, machines, makespan)
+end
+
+function cauchyTestCaseLowerBound(jobs, machines, maxJobTime)
+  time = rand(Cauchy(100, 100), jobs)
+  timeInt = map(x -> convert(Int64, abs(round(x))), time)
+  makespan = convert(Int64, round(linearProgrammingLowerBound(timeInt, machines)))
+  return TestCases.TestCase(timeInt, machines, makespan)
+end
+
 
 
 end
